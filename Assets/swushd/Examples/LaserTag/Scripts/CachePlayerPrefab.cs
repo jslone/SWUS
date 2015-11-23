574307fefcb4353eb55eb55283634cdc455286e0
Gkxd <gkxd@users.noreply.github.com>
2015-11-23 10:53:02 -0500

    Implementing static sync scheme

---
+++ Assets/swushd/Examples/LaserTag/Scripts/CachePlayerPrefab.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class CachePlayerPrefab : NetworkBehaviour {
+    public override void OnStartLocalPlayer() {
+        base.OnStartLocalPlayer();
+
+        StaticSyncUI.SetPlayer(this);
+    }
+
+    void Start() {
+        Debug.Log("You are player " + playerControllerId);
+    }
+
+    public void resetPosition() {
+        Debug.Log("Resetting Position");
+        transform.position = playerControllerId * new Vector3(0.2f, 0, 0);
+        transform.rotation = Quaternion.identity;
+    }
+}
---
+++ Assets/swushd/Examples/LaserTag/Scripts/StaticSyncUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StaticSyncUI : MonoBehaviour {
+
+    private static StaticSyncUI instance;
+    private CachePlayerPrefab player;
+
+    public Text buttonText;
+
+    void Awake() {
+        if (!instance) {
+            instance = this;
+        }
+    }
+
+    public void ResetPlayer() {
+        if (player) {
+            player.resetPosition();
+        }
+    }
+
+    public static void SetPlayer(CachePlayerPrefab player) {
+        instance.player = player;
+        instance.buttonText.text = "Reset Position (" + player.playerControllerId + ")";
+    }
+}
